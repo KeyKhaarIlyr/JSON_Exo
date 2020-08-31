45d27f93ccf9c27e740ec9572da80505e412eeea
Key <pereira.cabral.kalvin@gmail.com>
2020-08-31 16:26:45 +0200

    Trying to fix the layer

--- index.php
+++ index.php
@@ -12,17 +12,14 @@ include 'src/rooter.php';
 </head>
 
 <body>
-	<main class="d-flex justify-content-around">
-		<h1>Exo JSON !</h1>
+	<main class="d-flex row justify-content-around">
+		<h1 class="col-12">Exo JSON !</h1>
 		<?php
 
 		for ($i = 0; $i < 6; $i++) {
-			echo '<div class="card mb-3">
+			echo '<div class="card col-3 m-1">
 			<h3 class="card-header"> ' .
-				getInfo("firstName", $i) . ' '
-				.
-				getInfo("lastName", $i) . '
-				?>
+				getInfo("firstName", $i) . ' ' . getInfo("lastName", $i) . '
 			</h3>
 			<ul class="list-group list-group-flush">
 				<li class="list-group-item">Gender: 
